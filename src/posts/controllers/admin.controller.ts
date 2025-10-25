56fe1d0bee7bce8ba14f5c6bd81560df58c226ce
Adam Kuzniarski <AdamKuzniarski@users.noreply.github.com>
2025-10-25 18:20:36 +0200

    add crud

--- src/posts/controllers/admin.controller.ts
+++ src/posts/controllers/admin.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Delete, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { PostsService } from '../posts.service';
+import { CreatePostDto } from '../dto/create-post.dto';
+import{ UpdatePostDto } from '../dto/update-post.dto';
+
+
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+    constructor(private readonly postsService: PostsService) {}
+
+@Get()
+getAllAdminPosts() {
+    return this.postsService.findAllAdmin();
+}
+@Get(':id')
+getOneAdminPost(@Param('id', ParseIntPipe) id: number){
+return this.postsService.findOneByIdAdmin(id)
+}
+}
