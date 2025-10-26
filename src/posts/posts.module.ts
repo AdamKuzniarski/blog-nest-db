3dc061615500eb402201d4002153af8c8d25467e
Adam Kuzniarski <adam.kuzniarski@gmx.de>
2025-10-26 12:43:30 +0100

    add import a. exports in posts.module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostEntity } from 'src/entity/posts/post.entity';
 import { PostsService } from './posts.service';
 import { PublicController } from './controllers/public.controller';
 import { AdminController } from './controllers/admin.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PostEntity])],
   providers: [PostsService],
-  controllers: [PublicController, AdminController]
+  controllers: [PublicController, AdminController],
+  exports: [PostsService],
 })
 export class PostsModule {}
