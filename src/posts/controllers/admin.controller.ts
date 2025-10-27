cffa7b4224bef29b445631c73389c5eb7297c451
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-10-27 08:35:09 +0100

    prettier adds the tabs

--- src/posts/controllers/admin.controller.ts
+++ src/posts/controllers/admin.controller.ts
@@ -1,38 +1,45 @@
-import { Controller, Body, Delete, Get, Param, ParseIntPipe, Post, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Patch,
+} from '@nestjs/common';
 import { PostsService } from '../posts.service';
 import { CreatePostDto } from '../dto/create-post.dto';
-import{ UpdatePostDto } from '../dto/update-post.dto';
-
-
+import { UpdatePostDto } from '../dto/update-post.dto';
 
 @Controller('admin/posts')
 export class AdminController {
-    constructor(private readonly postsService: PostsService) {}
+  constructor(private readonly postsService: PostsService) {}
 
-@Get()
-getAllAdminPosts() {
+  @Get()
+  getAllAdminPosts() {
     return this.postsService.findAllAdmin();
-}
-@Get(':id')
-getOneAdminPost(@Param('id', ParseIntPipe) id: number){
-return this.postsService.findOneByIdAdmin(id)
-}
+  }
+  @Get(':id')
+  getOneAdminPost(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.findOneByIdAdmin(id);
+  }
 
-@Post()
-createOneAdminPost(@Body() createPostDto: CreatePostDto){
-    return this.postsService.createOne(createPostDto)
-}
+  @Post()
+  createOneAdminPost(@Body() createPostDto: CreatePostDto) {
+    return this.postsService.createOne(createPostDto);
+  }
 
-@Patch(':id')
-updateOneAdminPost(
+  @Patch(':id')
+  updateOneAdminPost(
     @Param('id', ParseIntPipe) id: number,
-    @Body() updatePostDto: UpdatePostDto){
-    return this.postsService.updateOneById(id, updatePostDto)
-}
+    @Body() updatePostDto: UpdatePostDto,
+  ) {
+    return this.postsService.updateOneById(id, updatePostDto);
+  }
 
-@Delete(':id')
-deleteOneAdminPost(@Param('id', ParseIntPipe) id: number){
-    return this.postsService.deleteOneById(id)
-}
-
+  @Delete(':id')
+  deleteOneAdminPost(@Param('id', ParseIntPipe) id: number) {
+    return this.postsService.deleteOneById(id);
+  }
 }
